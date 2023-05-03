022f0bb8d944fba09182fa090fe62fcef497a4e2
At Tafani Fillah <63786515+lzddb@users.noreply.github.com>
2023-05-03 12:07:34 +0700

    Update Perkalian.php

--- Perkalian.php
+++ Perkalian.php
@@ -10,7 +10,7 @@
 	<?php
 		$angka1 = 200;
 		$angka2 = 15;
-        $angka3 =  100;
+        	$angka3 =  100;
 		$jumlah = ($angka1 * $angka2) / $angka3;
 		
 		echo "Hasil dari penghitungan (" . $angka1 . " * " . $angka2 . ") / " . $angka3 . " adalah " . $jumlah . ".";
