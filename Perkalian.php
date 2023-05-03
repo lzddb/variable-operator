ed82249e00284983f3726c4e55cbdbbee3a3be8d
lzddb <attafanif@gmail.com>
2023-05-03 12:03:21 +0700

    Penghitungan Perkalian dam Pembagian

---
+++ Perkalian.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Perkalian</title>
+</head>
+<body>
+	<?php
+		$angka1 = 200;
+		$angka2 = 15;
+        $angka3 =  100;
+		$jumlah = ($angka1 * $angka2) / $angka3;
+		
+		echo "Hasil dari penghitungan (" . $angka1 . " * " . $angka2 . ") / " . $angka3 . " adalah " . $jumlah . ".";
+	?>
+</body>
+</html>
