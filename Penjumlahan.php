cbe6df101cf84cb02d636c38a242d0a5c049a7e9
lzddb <attafanif@gmail.com>
2023-05-03 12:00:33 +0700

    Penjumlahan dalam bahasa PHP

---
+++ Penjumlahan.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Penjumlahan</title>
+</head>
+<body>
+	<?php
+		$angka1 = 198;
+		$angka2 = 134;
+        $angka3 =  76;
+		$jumlah = $angka1 + $angka2 + $angka3;
+		$teks = 'Ketiga variabel bila dijumlah menjadi: ';
+		
+		echo ($teks . $jumlah);
+	?>
+</body>
+</html>
